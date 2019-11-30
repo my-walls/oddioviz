304fcefbbe19af636b1117bb79b078f58c8489df
Connor Walls <connor.walls@tweed.com>
2019-11-30 14:58:57 -0500

    Stuff

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import sys
 import terminalplot as tp
 from scipy import signal
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.ion()
+
 chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 1
@@ -24,8 +29,17 @@ stream = p.open(format=sample_format,
 # Store data in chunks for 3 seconds
 for i in range(0, int(fs / chunk * seconds)):
     data = list(stream.read(chunk, exception_on_overflow=False))
-    f, P = signal.periodogram(data, fs)
-    tp.plot(f.tolist(), P.tolist(), rows=50, columns=150)
+    f, Pxx_spec = signal.periodogram(data, fs, 'flattop', scaling='spectrum')
+
+    plt.semilogy(f, np.sqrt(Pxx_spec))
+    plt.ylim([1e-4, 1e1])
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('Linear spectrum [V RMS]')
+    plt.draw()
+    plt.pause(0.0001)
+    plt.clf()
+
+    # tp.plot(f.tolist(), P.tolist(), rows=50, columns=150)
 
 # Stop and close the stream
 stream.stop_stream()
